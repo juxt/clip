a20823138d53d9814cf1a08b1339a30545e5616e
Dominic Monroe <dmc@juxt.pro>
2019-10-22 18:54:30 +0100

    Add impl namespace
    Moves most of the implementation into a private namespace, this allows
    creating a more focused core API which can focus on assembling the
    pieces provided by the impl namespace.
    
    Over time, I expect parts of the impl to move into a documented
    namespace of some kind to allow for alternative assemblies of systems.

--- src/io/dominic/high/core.clj
+++ src/io/dominic/high/core.clj
@@ -1,482 +1,83 @@
 (ns io.dominic.high.core
-  (:require
-    [clojure.set :as set]))
+  (:require [io.dominic.high.impl.core :as impl]))
 
-(defn- sccs
-  "Returns a topologically sorted list of strongly connected components.
-  Tarjan's algorithm."
-  ([g] (sccs g []))
-  ([g sccs-init]
-   (let [strong-connect
-           (fn strong-connect [acc v]
-             (let [acc (-> acc
-                           (assoc-in [:idxs v] (:idx acc))
-                           (assoc-in [:low-links v] (:idx acc))
-                           (update :idx inc)
-                           (update :S conj v)
-                           (assoc-in [:on-stack v] true))
-                   acc (reduce
-                         (fn [acc w]
-                           (cond
-                             (not (get-in acc [:idxs w]))
-                               (let [acc (strong-connect acc w)]
-                                 (update-in acc
-                                            [:low-links v]
-                                            min
-                                            (get-in acc [:low-links w])))
-                             (get-in acc [:on-stack w])
-                               (update-in acc
-                                          [:low-links v]
-                                          min
-                                          (get-in acc [:idxs w]))
-                             :else acc))
-                         acc
-                         (get g v))]
-               (if (= (get-in acc [:idxs v]) (get-in acc [:low-links v]))
-                 (let [[S on-stack scc] (loop [S (:S acc)
-                                               on-stack (:on-stack acc)
-                                               scc #{}]
-                                          (let [w (peek S)
-                                                S (pop S)
-                                                on-stack (dissoc on-stack w)
-                                                scc (conj scc w)]
-                                            (if (= v w)
-                                              [S on-stack scc]
-                                              (recur S on-stack scc))))]
-                   (-> acc
-                       (assoc :S S
-                              :on-stack on-stack)
-                       (update :sccs conj scc)))
-                 acc)))]
-     (:sccs
-       (reduce (fn [acc v]
-                 (if-not (contains? (:idxs acc) v) (strong-connect acc v) acc))
-         {:S ()
-          :idx 0
-          :sccs sccs-init}
-         (keys g))))))
+(defn- safely-derive-parts
+  [components init]
+  (let [g (impl/system-dependency-graph components)
+        sccs (impl/sccs g init)]
+    (if-let [errors (seq (impl/dependency-errors sccs g))]
+      (throw
+        (ex-info
+          (apply
+            str
+            "Could not construct execution order because:\n  "
+            (interpose "\n  " (map impl/human-render-dependency-error errors)))
+          {:components components
+           :errors errors
+           :g g
+           :sccs sccs}))
 
-(defn- cycles
-  [sccs g]
-  (filter #(or (>= (count %) 2)
-               (get-in g [(first %) (first %)]))
-          sccs))
+      [g (map #(find components (first %)) sccs)])))
 
-(defn- system-dependency-graph
-  [system]
-  (let [ref? #(= 'high/ref (and (coll? %) (first %)))
-        ref-to second]
-    (into {}
-          (map (fn [[k v]]
-                 [k (set (map ref-to (filter ref? (tree-seq coll? seq (:start v)))))])
-               system))))
+(defn- ->executor
+  [executor]
+  (if (fn? executor)
+    executor
+    (requiring-resolve executor)))
 
-(defn- dependency-errors
-  [sccs g]
-  (concat
-    (mapcat
-      (fn [[k v]]
-        (seq
-          (map (fn [does-not-exist]
-                 {:type :missing
-                  :from k
-                  :to does-not-exist})
-               (remove #(contains? g %) v))))
-      g)
-    (map (fn [cycle] {:type :cycle :between cycle})
-         (cycles sccs g))))
-
-(defn- human-render-dependency-error
-  [dependency-error]
-  (case (:type dependency-error)
-    :missing
-    (str (:from dependency-error) " depends on " (:to dependency-error)
-         ", but " (:to dependency-error) " doesn't exist.")
-
-    :cycle
-    (str "There's a circular dependency between "
-         (apply str (interpose
-                      " -> "
-                      (concat
-                        (reverse (:between dependency-error))
-                        [(first (reverse (:between dependency-error)))]))))
-
-    (pr-str dependency-error)))
-
-(comment
-  (let [bad-system '{:a {:start (high/ref :does-not-exist)}
-                     :b {:start (high/ref :c)}
-                     :c {:start (high/ref :b)}
-                     
-                     :d {:start (high/ref :e)}
-                     :e {:start (high/ref :f)}
-                     :f {:start (high/ref :d)}}]
-    (map
-      (or #_identity human-render-dependency-error)
-      (dependency-errors
-        (sccs (system-dependency-graph bad-system))
-        (system-dependency-graph bad-system)))))
-
-(comment
-  (def system
-    '{:db {:start (hikari-cp.core/make-datasource
-                   {:jdbc-url
-                    "jdbc:neo4j:bolt://host:port/?username=neo4j,password=xxxx,debug=true"})
-          ;; No need to specify stop, implements java.io.Closeable
-          }
-
-     :db2 {:start
-           (crux.api/start-standalone-node
-             {:kv-backend "crux.kv.rocksdb.RocksKv"
-              :event-log-dir "data/eventlog-1"
-              :db-dir "data/db-dir-1"
-              :backup-dir "checkpoint"})
-           ;; No need to specify stop, implements java.io.Closeable
-           }
-     :yada {:start (yada.yada/listener
-                     (high/ref :routes)
-                     {:port 3000})
-
-            :stop ((:close this))}
-
-     :routes {:start
-              (myapp.routes/routes
-                (high/ref :db)
-                (high/ref :db2)
-                (high/ref :does-not-exist))
-              ;; :stop not required, has no stop lifecycle
-              }
-     })
-
-  (let [g (system-dependency-graph system)]
-    (dependency-errors (sccs g) g))
-
-  (let [g (system-dependency-graph system)]
-    (cycles (sccs g) g))
-  )
-
-(defn- namespace-symbol
-  "Returns symbol unchanged if it has a namespace, or with clojure.core as it's
-  namespace otherwise."
-  [sym]
-  (if (namespace sym)
-    sym
-    (symbol "clojure.core" (name sym))))
-
-(defn- evaluate-pseudo-clojure
-  ([x]
-   (cond
-     (symbol? x)
-     ((requiring-resolve (namespace-symbol x)))
-     (sequential? x)
-     (apply (if (symbol? (first x))
-              (requiring-resolve (namespace-symbol (first x)))
-              (evaluate-pseudo-clojure (first x)))
-            (map evaluate-pseudo-clojure (rest x)))
-     :else x))
-
-  ([x implicit-target]
-   (cond
-     (symbol? x)
-     ((requiring-resolve (namespace-symbol x)) implicit-target)
-
-     (keyword? x)
-     (get implicit-target x)
-
-     :else (evaluate-pseudo-clojure x))))
-
-(defn- ref?
-  [x]
-  (= 'high/ref (and (coll? x) (first x))))
-
-(def ^:private ref-to second)
-
-(defn- resolver
-  [x p? lookup]
-  (clojure.walk/postwalk
-    (fn [x]
-      (if (p? x) (lookup x) x))
-    x))
-
-(defn- resolve-refs
-  [x system-config running-system]
-  (resolver x
-            ref?
-            (fn [ref-list]
-              (let [to (ref-to ref-list)]
-                (cond->> (get running-system to)
-
-                  (get-in system-config [to :resolve])
-                  (evaluate-pseudo-clojure
-                    (get-in system-config [to :resolve])))))))
-
-(defn- pre-starting
-  [rf]
-  (fn
-    ([running-system system-config id value]
-     (let [component (get system-config id)]
-       (when (contains? component :pre-start)
-         (evaluate-pseudo-clojure
-           (resolve-refs
-             (get component :pre-start)
-             system-config
-             running-system))))
-     (try
-       (rf running-system system-config id value)
-       (catch Throwable e
-         (throw
-           (ex-info
-             "Error while creating system"
-             {:partial-system running-system}
-             e)))))))
-
-(defn- starting
-  [rf]
-  (fn
-    ([running-system system-config id _]
-     (let [component (get system-config id)
-           resolved-start
-           (resolve-refs (get component :start)
-                         system-config
-                         running-system)]
-       (try
-         (rf running-system
-             system-config
-             id
-             (evaluate-pseudo-clojure resolved-start))
-         (catch Throwable e
-           (throw
-             (ex-info
-               "Error while creating system"
-               {:partial-system running-system}
-               e))))))))
-
-(defn- post-starting
-  [rf]
-  (fn
-    ([running-system system-config id started]
-     (try
-       (let [component (get system-config id)]
-         (when (contains? component :post-start)
-           (evaluate-pseudo-clojure
-             (-> (get component :post-start)
-                 (resolve-refs system-config running-system)
-                 (resolver #(= 'this %) (constantly started)))
-             started))
-         (rf running-system system-config id started))
-       (catch Throwable e
-         (throw
-           (ex-info
-             "Error while creating system"
-             {:partial-system running-system}
-             e)))))))
-
-(defn- stop!
-  [inst stop-code]
-  (cond
-    stop-code
-    (evaluate-pseudo-clojure
-      (-> stop-code
-          (resolver #(= 'this %) (constantly inst)))
-      inst)
-    (isa? (class inst) java.io.Closeable)
-    (.close ^java.io.Closeable inst)))
-
-(defn- stopping
-  [rf]
-  (fn
-    ([running-system system-config id v]
-     (try
-       (stop! (get running-system id)
-              (get-in system-config [id :stop]))
-       (rf running-system system-config id v)
-       (catch Throwable e
-         (throw
-           (ex-info
-             "Error while stopping system"
-             {:partial-system running-system}
-             e)))))))
-
-(defn- run
-  ([xf system-config component-chain]
-   (run xf {} system-config component-chain))
-  ([xf init system-config component-chain]
-   (let [f (xf (fn [acc _ id v] (conj acc [id v])))]
-     (reduce (fn [init [id component]]
-               (f init system-config id (get init id)))
-             init
-             component-chain))))
-
-(defn- promesa-run
-  ([xfs system-config component-chain]
-   (promesa-run xfs system-config ((requiring-resolve 'promesa.core/promise) {}) component-chain))
-  ([xfs system-config init component-chain]
-   (let [promise (requiring-resolve 'promesa.core/promise)
-         then (requiring-resolve 'promesa.core/then)
-         promesa-wait
-         (fn [rf]
-           (fn [acc system-config id value]
-             (-> value (then #(rf acc system-config id %)))))
-         xf (apply comp (interpose promesa-wait xfs))
-         f (xf
-             (fn [acc system-config id value]
-               (-> value
-                   (then (fn [value]
-                           (assoc acc id value))))))]
-     (reduce
-       (fn [prom-chain [id component]]
-         (-> prom-chain
-             (then (fn [acc] (f acc system-config id (get acc id))))))
-       init
-       component-chain))))
-
-(comment
-  (def promesa-system
-    '{foo {:start (io.dominic.high.core/StatefulThing)
-           :pre-start (println "aaaa")}
-      :a {:pre-start (println 10)
-          :start (promesa.core/resolved 5)}
-      :num {:start 10
-            :post-start (println "init-:num" (inc this))
-            :stop (println "num-->" this)}
-      :b {:start (promesa.core/resolved (inc (high/ref :a)))}
-      :c {:start (+ (high/ref :a) (high/ref :b))}})
-
-  @(promesa-run
-     [stopping]
-     promesa-system
-     (promesa-run [pre-starting starting post-starting]
-                  promesa-system
-                  (component-chain promesa-system))
-     (reverse-component-chain promesa-system))
-  )
-
-(defn- component-chain
-  [system]
-  (map #(find system (first %))
-       (sccs (system-dependency-graph system))))
-
-(defn- reverse-component-chain
-  [system]
-  (map #(find system (first %))
-       (sccs (system-dependency-graph system) ())))
-
-(comment
-  (defn StatefulThing
-    []
-    (reify
-      java.io.Closeable
-      (close [this]
-        (println  "Closing a stateful thing"))))
-  (def system2
-    '{:a {:start (io.dominic.high.core/StatefulThing)
-          :pre-start (println "aaaa")}
-      :num {:start 10
-            :stop (println "num-->" this)}
-      :b {:start (inc (high/ref :num))
-          :pre-start (println "bbbb")
-          :post-start  (println "postpostpost" this)}})
-
-  (run starting
-       system2
-       (component-chain system2))
-
-  (run
-    stopping
-    (run (comp pre-starting starting post-starting)
-         system2
-         (component-chain system2))
-    system2
-    (reverse-component-chain system2)))
-
-(comment
-  (cycles {:a #{:b}
-           :b #{:a}
-           :c #{:b :a}})
-  (cycles (sccs {:a #{:b}
-                 :b #{:a}
-                 :c #{:b :a}}))
-
-  (let [g {1 #{2}
-           2 #{3}
-           3 #{1}
-           4 #{3 2 5}
-           5 #{4 6}
-           6 #{7 3}
-           7 #{6}
-           8 #{7 8}}]
-    (cycles (sccs g) g)))
-
-(defn- starting-f
+(defn start
+  "Takes a system config to start.  Returns a running system where the keys map
+  to the started component.  Runs the :pre-start, :start and :post-start keys
+  in that order.
+  
+  :pre-start and :start may contains references, and will be executed without
+  an implicit target.
+  :post-start may also contain references, but will have the started component
+  as an implict target.  This means that a symbol on it's own will work instead
+  of requiring a code form, in addition the anaphoric variable `this` is
+  available to refer to the started component."
   [system-config]
-  (fn [[k {:keys [start]}]]
-    (fn [rf acc]
-      (rf acc
-          k
-          (evaluate-pseudo-clojure
-            (resolve-refs start system-config acc))))))
+  (let [{:keys [components executor]
+         :or {executor impl/exec-queue}} system-config
+        executor (->executor executor)
+        [g component-chain] (safely-derive-parts components [])]
+    (executor
+      (for [component component-chain
+            f [(impl/pre-starting-f components)
+               (impl/starting-f components)
+               (impl/post-starting-f components)]]
+        (f component))
+      {})))
 
-(defn- pre-starting-f
-  [system-config]
-  (fn [[k {:keys [pre-start]}]]
-    (fn [rf acc]
-      (when pre-start
-        (evaluate-pseudo-clojure
-          (resolve-refs pre-start system-config acc)))
-      acc)))
+(defn stop
+  "Takes a system config to stop.
 
-(defn- post-starting-f
-  [system-config]
-  (fn [[k {:keys [post-start]}]]
-    (fn [rf acc]
-      (when post-start
-        (evaluate-pseudo-clojure
-          (-> post-start
-              (resolve-refs system-config acc)
-              (resolver #(= 'this %) (constantly (get acc k))))
-          (get acc k)))
-      acc)))
-
-(defn- stopping-f
-  [[k {:keys [stop]}]]
-  (fn [rf acc]
-    (stop! (get acc k) stop)
-    (-> acc
-        (rf (str k "-stop1") :value1)
-        (rf (str k "-stop2") :value2))))
-
-(defn- exec-queue
-  ([q] (exec-queue q {}))
-  ([q init]
-   (reduce (fn [acc f] (f assoc acc)) init q)))
-
-(defn- promesa-exec-queue
-  ([q] (promesa-exec-queue
-         q
-         ((requiring-resolve 'promesa.core/resolved) {})))
-  ([q init]
-   (let [all (requiring-resolve 'promesa.core/all)
-         then (requiring-resolve 'promesa.core/then)]
-     (reduce
-       (fn [*acc f]
-         (-> *acc
-             (pr/then (fn [acc]
-                        (f (fn [acc k v]
-                             (-> (all [acc k v])
-                                 (then #(apply assoc %))))
-                           acc)))))
-       init
-       q))))
+  Runs the :stop key, which may not contain references, and will be executed
+  with the started component as an implict target.  If no :stop is provided and
+  the target is AutoClosable then .close will be called on it, otherwise
+  nothing."
+  [system-config running-system]
+  (let [{:keys [components executor]
+         :or {executor impl/exec-queue}} system-config
+        executor (->executor executor)
+        [g component-chain] (safely-derive-parts components ())]
+    (executor
+      (map impl/stopping-f component-chain)
+      running-system)))
 
 (comment
-  (exec-queue
-    (concat (map (starting-f system2) (component-chain system2))
-            (map stopping-f (reverse-component-chain system2))))
+  (start
+    {:components '{:foo {:start (high/ref :bar)}
+                   :bar {:start (high/ref :foo)}
+                   :baz {:start (high/ref :baz)}
+                   :foob {:start (high/ref :nowhere)}}
+     :executor impl/exec-queue})
 
-  @(promesa-exec-queue
-     (concat
-       (for [f [(pre-starting-f system2)
-                (starting-f system2)]
-             component (component-chain system2)]
-         (f component))
-       (map stopping-f (reverse-component-chain system2)))))
+  (let [system-config {:components '{:foo {:pre-start (prn "init:foo")
+                                           :start 1
+                                           :post-start prn
+                                           :stop prn}
+                                     :bar {:start (inc (high/ref :foo))
+                                           :stop prn}}
+                       :executor impl/promesa-exec-queue}]
+    (stop system-config (start system-config))))
