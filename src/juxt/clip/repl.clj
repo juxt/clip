9c56ad5b65a74929e83185773e95567b62f2af0c
Dominic Monroe <monroef4@googlemail.com>
2020-06-01 22:37:30 +0100

    Add support for async systems in .repl
    This allows using manifold/promesa with clip's .repl namespace.
    
    Fix #10

--- src/juxt/clip/repl.clj
+++ src/juxt/clip/repl.clj
@@ -16,37 +16,55 @@
 (defn- stop-system
   [system]
   (when system
-    (let [system-config (::system-config (meta system))]
-      (clip/stop system-config system)
+    (let [{::keys [deref] :as system-config} (::system-config (meta system))]
+      (deref (clip/stop system-config system))
       system)))
 
 (defn- start-system
   [system-config]
-  (vary-meta
-    (try
-      (clip/start system-config)
-      (catch Throwable t
-        (if-let [system (::clip/system (ex-data t))]
-          ;; Partially started system found, we should call stop on it to clean
-          ;; up.
-          (do
-            (try
-              (clip/stop system-config system)
-              (catch Throwable stop-t
-                (throw
-                  (ex-info
-                    "Exception thrown while starting system.  Exception also thrown while stopping system."
-                    {:stop-exception stop-t}
-                    t))))
-            (throw (ex-cause t)))
-          (throw t))))
-    merge {::system-config system-config}))
+  (let [{::keys [deref]} system-config]
+    (vary-meta
+      (try
+        (deref (clip/start system-config))
+        (catch Throwable t
+          (if-let [system (::clip/system (ex-data t))]
+            ;; Partially started system found, we should call stop on it to clean
+            ;; up.
+            (do
+              (try
+                (clip/stop system-config system)
+                (catch Throwable stop-t
+                  (throw
+                    (ex-info
+                      "Exception thrown while starting system.  Exception also thrown while stopping system."
+                      {:stop-exception stop-t}
+                      t))))
+              (throw (ex-cause t)))
+            (throw t))))
+      merge {::system-config system-config})))
+
+(defn- force*
+  "Like force, but for anything that can be deref'd"
+  [ref]
+  (if
+    (or (instance? clojure.lang.IBlockingDeref ref)
+        (instance? java.util.concurrent.Future ref))
+    (deref ref)
+    ref))
+
+(defn- ->deref
+  [deref]
+  #?(:cljs deref
+     :default
+     (if (fn? deref)
+       deref
+       (requiring-resolve deref))))
 
 (defn set-init!
   "Set the initializer to init-fn.  Should be a function which takes no
   arguments and returns the system config to use for the REPL."
   [init-fn]
-  (alter-var-root #'initializer (constantly init-fn)))
+  (alter-var-root #'initializer (constantly #(update (merge {::deref force*} (init-fn)) ::deref ->deref))))
 
 (defn start
   "Stops any existing systems and starts a new one calling the initializer set
