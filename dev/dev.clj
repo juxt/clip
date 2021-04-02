b0ec15949e54c62d8c9be406f05d24dc62a679b0
Dominic Monroe <monroef4+github@gmail.com>
2021-04-02 16:30:16 +0100

    Add example of reloadable system

--- dev/dev.clj
+++ dev/dev.clj
@@ -2,7 +2,8 @@
   (:require
     [datomic.api :as d]
     [yada.yada :as yada]
-    [juxt.clip.core :as h]))
+    [juxt.clip.core :as h]
+    [juxt.clip.repl.yada :as juxt.clip.repl.yada]))
 
 (defn seed-conn
   [conn]
@@ -22,16 +23,25 @@
            '[*]
            :test))}}}))
 
+(defn ring-handler
+  [db]
+  (fn [req]
+    {:status 200
+     :body "Hello?  How are you"}))
+
 (def system-config
   {:components
    {:db {:pre-start `(d/create-database "datomic:mem://newdb")
          :start `(d/connect "datomic:mem://newdb")
          :post-start `seed-conn}
-    :handler {:start `(find-seed-resource (clip/ref :db))}
+    ;; :handler {:start `(find-seed-resource (clip/ref :db))}
+    :handler {:start `(ring-handler (clip/ref :db))}
     :http {:start `(yada/listener (clip/ref :handler))
            :stop '((:close this))
            :resolve :server}
-    :foo {:start '(clip/ref :http)}}})
+    :foo {:start '(clip/ref :http)}}
+   :reloads {yada.resource.Resource juxt.clip.repl.yada/reloadable-resource
+             clojure.lang.Fn juxt.clip.repl/relodable-fn}})
 
 (def rf-system
   (#'h/exec-queue
@@ -53,4 +63,17 @@
     rf-system)
 
   (throw (ex-info "foo" {}))
+
+  (def e *e)
+
+  (ex-data e)
+
+  (ex-data (ex-cause e))
+
+  (:port (:http juxt.clip.repl/system))
+
+  (juxt.clip.repl/set-init! #(deref #'system-config))
+  (juxt.clip.repl/go)
+  (juxt.clip.repl/stop)
+  (juxt.clip.repl/reset)
   )
